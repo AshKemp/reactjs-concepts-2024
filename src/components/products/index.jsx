0f2c371f94360187d0a1d38d79ebfc8b9bfa34f5
AshKemp <kmuraliashwin@gmail.com>
2024-07-27 20:12:30 +0530

    conditional rendering

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -6,13 +6,30 @@ import "./style.css";
 
 function ProductList(props) {
   const { name, city, listOfProducts } = props;
+  const flag = true;
+  //   function renderTextBlock(flag) {
+  //     return flag ? (
+  //       <h4>
+  //         Name is {name}, he/she belongs to the city {city}
+  //       </h4>
+  //     ) : (
+  //       <h4> Hello World</h4>
+  //     );
+  //   }
+
+  const renderTextBlock = flag ? (
+    <h4>
+      Name is {name}, he/she belongs to the city {city}
+    </h4>
+  ) : (
+    <h4> Hello World</h4>
+  );
+
   return (
     <div>
       <h3 className="title">E-Commerce Project</h3>
       {/* <ProductItem /> */}
-      <h4>
-        Name is {name}, he/she belongs to the city {city}
-      </h4>
+      {renderTextBlock}
       <ul>
         {listOfProducts.map((product, index) => (
           <ProductItem product={product} key={index} />
