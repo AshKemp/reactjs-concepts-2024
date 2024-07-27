d2f30104765e207373408d9cdd883d8fcca977c2
AshKemp <kmuraliashwin@gmail.com>
2024-07-27 18:39:43 +0530

    create functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import ClassBasedComponent from "./components/class-based-component";
+import FunctionalComponent from "./components/functional-component";
 
 function App() {
   return (
     <>
       <h1>React JS Concepts 2024</h1>
       <ClassBasedComponent />
+      <FunctionalComponent />
     </>
   );
 }
