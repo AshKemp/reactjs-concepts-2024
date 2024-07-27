0e3499df78947e9c3dcff6d179ff645e68623b2f
AshKemp <kmuraliashwin@gmail.com>
2024-07-27 18:36:06 +0530

    create class based component

---
+++ src/components/class-based-component.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class ClassBasedComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Class Based Component</h4>
+      </div>
+    );
+  }
+}
+
+export default ClassBasedComponent;
