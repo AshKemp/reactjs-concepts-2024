43094681ea0c66869d2543a2b21ca25dda120e1c
AshKemp <kmuraliashwin@gmail.com>
2024-07-27 21:57:07 +0530

    life cycle methods in class based components

--- src/components/class-based-component.jsx
+++ src/components/class-based-component.jsx
@@ -6,7 +6,35 @@ class ClassBasedComponent extends Component {
     this.state = {
       showText: false,
       changeColor: false,
+      count: 0,
+      changeCountStyle: false,
     };
+  }
+
+  componentDidMount() {
+    console.log("This is called first time on page load");
+    this.setState({
+      showText: !this.state.showText,
+      changeColor: !this.state.changeColor,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log(prevProps, prevState);
+    if (
+      prevState &&
+      prevState.count !== this.state.count &&
+      this.state.count === 10
+    ) {
+      this.setState({
+        ...this.state,
+        changeCountStyle: true,
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("this is called when component gets destroyed");
   }
 
   handleClick = () => {
@@ -17,9 +45,16 @@ class ClassBasedComponent extends Component {
       changeColor: !changeColor,
     });
   };
+
+  handleCount = () => {
+    this.setState({
+      ...this.state,
+      count: this.state.count + 1,
+    });
+  };
   render() {
-    console.log(this.state);
-    const { showText, changeColor } = this.state;
+    // console.log(this.state);
+    const { showText, changeColor, count, changeCountStyle } = this.state;
     return (
       <div>
         {showText ? (
@@ -28,6 +63,15 @@ class ClassBasedComponent extends Component {
           </h4>
         ) : null}
         <button onClick={this.handleClick}>Toggle text</button>
+        <button onClick={this.handleCount}>Increase the count</button>
+        <h3
+          style={{
+            color: changeCountStyle ? "blue" : "black",
+            fontSize: changeCountStyle ? "30px" : "12px",
+          }}
+        >
+          Count is: {count}
+        </h3>
       </div>
     );
   }
