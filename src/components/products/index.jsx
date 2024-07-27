8f8fdc514c3ca1eb00b9725f1e3d657d3d847768
AshKemp <kmuraliashwin@gmail.com>
2024-07-27 18:50:59 +0530

    create nested components

---
+++ src/components/products/index.jsx
@@ -0,0 +1,13 @@
+import ProductItem from "./components/product-item";
+
+// const dummyProduct = ["Product 1", "Product 2", "Product 3"];
+function ProductList() {
+  return (
+    <div>
+      <h3>E-Commerce Project</h3>
+      <ProductItem />
+    </div>
+  );
+}
+
+export default ProductList;
