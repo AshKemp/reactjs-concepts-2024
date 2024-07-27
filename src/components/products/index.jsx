a67ec8387be3bb72519f5677a1a17aec13a35f7d
AshKemp <kmuraliashwin@gmail.com>
2024-07-27 18:55:36 +0530

    display list and add keys

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -1,11 +1,16 @@
-import ProductItem from "./components/product-item";
+// import ProductItem from "./components/product-item";
 
-// const dummyProduct = ["Product 1", "Product 2", "Product 3"];
+const dummyProduct = ["Product 1", "Product 2", "Product 3"];
 function ProductList() {
   return (
     <div>
       <h3>E-Commerce Project</h3>
-      <ProductItem />
+      {/* <ProductItem /> */}
+      <ul>
+        {dummyProduct.map((product, index) => (
+          <li key={index}>{product}</li>
+        ))}
+      </ul>
     </div>
   );
 }
