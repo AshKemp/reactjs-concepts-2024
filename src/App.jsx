be9833b9716a9a552bbbad4de0741d9775f70207
AshKemp <kmuraliashwin@gmail.com>
2024-07-27 20:52:24 +0530

    state and setState in class based components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import "./App.css";
-// import ClassBasedComponent from "./components/class-based-component";
+import ClassBasedComponent from "./components/class-based-component";
 // import FunctionalComponent from "./components/functional-component";
-import ProductList from "./components/products";
-const dummyProduct = ["Product 1", "Product 2", "Product 3"];
+// import ProductList from "./components/products";
+// const dummyProduct = ["Product 1", "Product 2", "Product 3"];
 
 function App() {
   return (
     <>
       <h1>React JS Concepts 2024</h1>
-      {/* <ClassBasedComponent />
-      <FunctionalComponent /> */}
-      <ProductList
+      {<ClassBasedComponent />}
+      {/* <FunctionalComponent />  */}
+      {/* <ProductList
         listOfProducts={dummyProduct}
         name={"Ashwin"}
         city={"Chennai"}
-      />
+      /> */}
     </>
   );
 }
--- src/components/class-based-component.jsx
+++ src/components/class-based-component.jsx
@@ -1,10 +1,33 @@
 import { Component } from "react";
 
 class ClassBasedComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showText: false,
+      changeColor: false,
+    };
+  }
+
+  handleClick = () => {
+    console.log("button clicked");
+    const { showText, changeColor } = this.state;
+    this.setState({
+      showText: !showText,
+      changeColor: !changeColor,
+    });
+  };
   render() {
+    console.log(this.state);
+    const { showText, changeColor } = this.state;
     return (
       <div>
-        <h4>Class Based Component</h4>
+        {showText ? (
+          <h4 style={{ color: changeColor ? "red" : "black" }}>
+            Class Based Component
+          </h4>
+        ) : null}
+        <button onClick={this.handleClick}>Toggle text</button>
       </div>
     );
   }
