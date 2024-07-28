22f6a1a5594fe8c0a7d0d950cdf3012b0ce75a60
AshKemp <kmuraliashwin@gmail.com>
2024-07-28 09:50:28 +0530

    useEffect example

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 // import ProductItem from "./components/product-item";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductItem from "./components/product-item";
 import "./style.css";
 
 function ProductList(props) {
   const { name, city, listOfProducts } = props;
   const [flag, setFlag] = useState(false);
+  const [count, setCount] = useState(0);
+  const [changeStyle, setChangeStyle] = useState(false);
   //   function renderTextBlock(flag) {
   //     return flag ? (
   //       <h4>
@@ -17,9 +19,24 @@ function ProductList(props) {
   //       <h4> Hello World</h4>
   //     );
   //   }
+  useEffect(() => {
+    setFlag(!flag);
+    console.log("run only once per page load");
+    return () => {
+      console.log("Component is unmounted");
+    };
+  }, []);
+
+  useEffect(() => {
+    if (count === 10) setChangeStyle(true);
+  }, [count]);
 
   const handleToggleText = () => {
     setFlag(!flag);
+  };
+
+  const handleToggleCount = () => {
+    setCount(count + 1);
   };
 
   const renderTextBlock = flag ? (
@@ -36,6 +53,18 @@ function ProductList(props) {
       {/* <ProductItem /> */}
       <button onClick={handleToggleText}>Toggle text</button>
       {renderTextBlock}
+      <div>
+        <button
+          style={{
+            backgroundColor: changeStyle ? "black" : "white",
+            color: changeStyle ? "white" : "black",
+          }}
+          onClick={handleToggleCount}
+        >
+          Toggle Count
+        </button>
+        <p>The count is {count}</p>
+      </div>
       <ul>
         {listOfProducts.map((product, index) => (
           <ProductItem product={product} key={index} />
