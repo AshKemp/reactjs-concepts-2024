d7bb4d7a1ad3bc9e84563956972a294de5653ba6
AshKemp <kmuraliashwin@gmail.com>
2024-07-28 11:28:07 +0530

    fetch data using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
+import Users from "./components/users";
 // import ClassBasedComponent from "./components/class-based-component";
 // import FunctionalComponent from "./components/functional-component";
-import ProductList from "./components/products";
-const dummyProduct = ["Product 1", "Product 2", "Product 3"];
+// import ProductList from "./components/products";
+// const dummyProduct = ["Product 1", "Product 2", "Product 3"];
 
 function App() {
   return (
@@ -10,13 +11,14 @@ function App() {
       <h1>React JS Concepts 2024</h1>
       {/* {<ClassBasedComponent />} */}
       {/* <FunctionalComponent />  */}
-      {
+      {/* {
         <ProductList
           listOfProducts={dummyProduct}
           name={"Ashwin"}
           city={"Chennai"}
         />
-      }
+      } */}
+      <Users />
     </>
   );
 }
---
+++ src/components/users/index.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+export default function Users() {
+  const [usersList, setUsersList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  async function fetchAllUsers() {
+    try {
+      const apiResponse = await fetch("https://dummyjson.com/users");
+      const result = await apiResponse.json();
+      console.log(result);
+      if (result?.users) {
+        setUsersList(result?.users);
+        setLoading(false);
+      } else {
+        setUsersList([]);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchAllUsers();
+  }, []);
+
+  if (loading) {
+    return <h1>Fetching users... Please wait!</h1>;
+  }
+  return (
+    <div>
+      <h1>All Users List</h1>
+      <ul>
+        {usersList && usersList.length > 0 ? (
+          usersList.map((user) => (
+            <li key={user.id}>
+              <p>
+                {user?.firstName} {user?.lastName}
+              </p>
+            </li>
+          ))
+        ) : (
+          <h4>No user is found</h4>
+        )}
+      </ul>
+    </div>
+  );
+}
